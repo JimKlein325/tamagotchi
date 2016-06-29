923ea7f961b95c06b41997b7c1f40e7e153dbf7a
Shradha Pulla and Jim Klein <pullashradha@gmail.com>
2016-06-29 13:28:32 -0700

    Add code in Pet.cs

--- Objects/Pet.cs
+++ Objects/Pet.cs
@@ -4,6 +4,73 @@ namespace Tamagotchi.Objects
 {
   public class Pet
   {
-
+    private string _name;
+    private int _food = 10;
+    private int _play = 10;
+    private int _sleep = 10;
+    private bool _isAlive = true;
+    private static List<Pet> _pet = new List<Pet> {};
+    public Pet (string name, int food, int play, int sleep)
+    {
+      _name = name;
+      _food = food;
+      _play = play;
+      _sleep = sleep;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetFood()
+    {
+      return _food;
+    }
+    public void SetFood(int newFood)
+    {
+      _food = newFood;
+    }
+    public void FeedPet()
+    {
+      _food ++;
+    }
+    public int GetPlay()
+    {
+      return _play;
+    }
+    public void SetPlay(int newPlay)
+    {
+      _play = newPlay;
+    }
+    public int GetSleep ()
+    {
+      return _sleep;
+    }
+    public void SetSleep(int newSleep)
+    {
+      _sleep = newSleep;
+    }
+    public static List<Pet> GetAll()
+    {
+      return _pet;
+    }
+    public void Save()
+    {
+      _pet.Add(this);
+    }
+    public void MakeTimePass()
+    {
+      if (_food > 0)
+      {
+        _food --;
+      }
+      else if (_food == 0 && _food <= 0)
+      {
+        this._isAlive = false;
+      }
+    }
   }
 }
